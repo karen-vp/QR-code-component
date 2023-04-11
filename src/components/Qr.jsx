160692047ac1aed87a8e8585432aa9ebd694c68c
karenvp <karenvalierpalmero@gmail.com>
2023-04-10 19:47:13 -0600

    feat: create the qr card component

---
+++ src/components/Qr.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function Qr() {
+  return (
+    <>
+        <article className='qr-card'>
+            <img src="../../public/img/image-qr-code.png" alt="qr-code-img" className='card-img' />
+            <h1>Improve your front-end skills by building projects</h1>
+            <div className='card-desc'>
+                <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
+        </article>
+    </>
+  )
+}
+
+export default Qr
+
+
+
