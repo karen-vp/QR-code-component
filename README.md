0a92c0607a4c4e5fad2f3fca7e6dcb08425b3533
karenvp <karenvalierpalmero@gmail.com>
2023-04-10 20:00:29 -0600

    fix: fix a image src

--- README.md
+++ README.md
@@ -18,6 +18,8 @@ This is a solution to the [QR code component challenge on Frontend Mentor](https
 
 ## Overview
 
+![](/src/assets/img/qr-component-preview.png)
+
 ### Screenshot
 
 
--- src/components/Qr.jsx
+++ src/components/Qr.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import qrimg from '../assets/img/image-qr-code.png'
 
 function Qr() {
   return (
     <>
         <article className='qr-card'>
-            <img src="../../public/img/image-qr-code.png" alt="qr-code-img" className='card-img' />
+            <img src={qrimg} alt="qr-code-img" className='card-img' />
             <h1>Improve your front-end skills by building projects</h1>
             <div className='card-desc'>
                 <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
